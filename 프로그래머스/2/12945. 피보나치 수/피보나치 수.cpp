57f7c56821d8380f00ced0141bb08977ec1f4873
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-01-03 22:30:09 +0900

    [level 2] Title: 피보나치 수, Time: 2.91 ms, Memory: 7.04 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int arr[100000];
+int fibo(int n){
+    
+    if(n == 0)
+        return  0;
+    if(n == 1 || n == 2)
+        return  1;
+    
+    if(arr[n]!=0)
+        return arr[n] % 1234567;
+    
+    else{
+        arr[n] = fibo(n-1) + fibo(n-2) % 1234567;
+    }
+    return arr[n] % 1234567;
+}
+int solution(int n) {
+    int answer = 0;
+    answer = fibo(n);
+    return answer;
+}
