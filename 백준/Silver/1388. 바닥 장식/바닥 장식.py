c170d94d3e22f7894091e1b5177bcc2a1f7b128f
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-01-07 13:18:27 +0900

    [Silver IV] Title: 바닥 장식, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,60 @@
+height, width = map(int, input().split())
+arr = []
+colCnt, rowCnt = 0, 0
+
+for _ in range(height):
+    temp = list(input())
+    arr.append(temp)
+
+#print(arr)
+
+for i in range(height):
+    col = 0
+    isWidth = False
+    
+    while col<width:
+        isWidth = False
+        # col-1 까지 -이면 col 증가
+        # 끝이 같다면 상관없지만, 다른 종류면 예외처리
+        while col+1<width and arr[i][col] =='-':
+            col+=1
+            isWidth = True
+
+        if col == width-1:
+            if isWidth == False and arr[i][col] =='-':
+                colCnt+=1
+
+        if isWidth:
+            colCnt+=1
+            isWidth = False
+
+        
+        col+=1
+    #colCnt+=tempColCnt
+    
+
+for i in range(width):
+    row = 0
+    isHeight = False
+    
+    while row<height:
+        isHeight = False
+        # row-1 까지 |이면 row 증가
+        # 끝이 같다면 상관없지만, 다른 종류면 예외처리        
+        while  row+1<height and arr[row][i] =='|':
+            row+=1
+            isHeight = True
+            
+        if row == height-1:
+            if isHeight == False and arr[row][i] =='|':
+                rowCnt+=1
+                
+        if isHeight:
+            rowCnt+=1
+            isHeight=False
+
+        row+=1
+    #rowCnt+=tempRowCnt
+
+#print(f"rowCnt: {rowCnt}, colCnt: {colCnt}")    
+print(rowCnt+colCnt)
