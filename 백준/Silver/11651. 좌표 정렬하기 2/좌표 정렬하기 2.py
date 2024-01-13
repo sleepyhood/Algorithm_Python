ecb8dabb21aec96cb3effd3de173fd38fc179014
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-01-13 12:34:33 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 368 ms, Memory: 56780 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,15 +1,22 @@
-N = int(input())
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+N = int(input().rstrip())
 row = N
 col = 2
 
 arr = [[0 for j in range(col)] for i in range(row)]
 
 for i in range(N):
-    x, y = map(int, input().split())
+    num = input().rstrip()
+    x, y = map(int, num.split())
+    
     arr[i][0] = x
     arr[i][1] = y
     
 sorted_list = sorted(arr, key=lambda x: (x[1], x[0]))
 
 for i in range(N):
-    print(f"{sorted_list[i][0]} {sorted_list[i][1]}")
+    print(f"{sorted_list[i][0]} {sorted_list[i][1]}\n")
