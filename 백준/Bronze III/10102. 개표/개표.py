21c8c52542eb37b09590251b48bf7a6eae9b7d16
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-02-16 13:00:39 +0900

    [Bronze III] Title: 개표, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze III/10102. 개표/개표.py
@@ -0,0 +1,15 @@
+V = int(input())
+votes = list(input())
+a, b = 0, 0
+
+for vote in votes:
+    if vote == 'A':
+        a+=1
+    else:
+        b+=1
+if a>b:
+    print('A')
+elif a<b:
+    print('B')
+else:
+    print('Tie')
