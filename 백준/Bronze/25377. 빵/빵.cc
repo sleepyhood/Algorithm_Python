42dfdbdca5c612d5c68f0fcffd5f1e039f15a470
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-05-13 14:02:17 +0900

    [Bronze IV] Title: 빵, Time: 0 ms, Memory: 2028 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.cc
+++ 백준/Bronze/25377. 빵/빵.cc
@@ -1,27 +1,38 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+#define MAXLEN 1001
 
-int main(){
+int a[MAXLEN];
+int b[MAXLEN];
+
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
+
     int n;
-    cin>>n;
+    cin >> n;
     int ans = 0;
-    
-    while(n--){
-        int a, b;
-        cin >> a>>b;
-        if(a<=b){    // 도착이 가능하다면
-            ans+=b-a;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i] >> b[i];
+    }
+
+    int idx = MAXLEN;
+    for (int i = 1; i <= n; i++) { // 빵이 들어오는 시간은 a이다, b보다 커야함
+        if (a[i] <= b[i]) {
+            idx = min(idx, b[i]);
         }
     }
-    if(ans){ // 빵을 한번이라도 샀다면
-        cout<<ans<<"\n";
+
+    if (idx == MAXLEN && a[1] > b[1]) {
+        cout << -1 << "\n";
     }
-    else
-        cout<<-1<<"\n";
-    
+    else {
+        cout << idx << "\n";
+    }
+
+
     return 0;
 }
