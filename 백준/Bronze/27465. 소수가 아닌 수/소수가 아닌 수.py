c1357a0db96f162271f2991f6c8ed783d49ae8cd
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-09-21 22:16:44 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 88 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
@@ -1,12 +1,3 @@
 n = int(input())
 
-for i in range(n, 10**9+1):
-    isPrime = True
-    for j in range(2, i):
-        if i % j == 0:
-            isPrime = False
-            break
-    if not isPrime: 
-        print(i)
-        break
-            
+print(10**9)
