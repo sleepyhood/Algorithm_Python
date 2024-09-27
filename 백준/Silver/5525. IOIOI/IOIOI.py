86a0b09345e00ce631971a665f761dea9494b34f
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-09-28 00:27:13 +0900

    [Silver I] Title: IOIOI, Time: 108 ms, Memory: 112136 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,35 +1,32 @@
+"""
+[백준]
+5525. IOIOI
+https://www.acmicpc.net/problem/5525
+"""
+
+import heapq
 import sys
 from collections import deque
+# 무한대를 의미하는 값으로 충분히 큰 수를 설정합니다.
+#INF = float('INF')
 
-input = sys.stdin.readline
-print = sys.stdout.write
+#input = sys.stdin.readline
+#print = sys.stdout.write
 
-N = int(input().rstrip())
-ioi = ''
-# N은 0의 개수, N+1개의 1의 개수와 이루어짐
+n = int(input())
+m = int(input())
+#li = list(map(int, input().split()))
+s = input()
 
-for i in range(N+N+1):
-    if i%2!=0:
-        ioi += 'O'
-    else:
-        ioi += 'I'       
-        
-M = int(input().rstrip())
-S = input().rstrip()    
+IOI = ""
 
-cnt = 0
-idx = 0
+for _ in range(n):
+    IOI+="IO"
+IOI+="I"
+#print(IOI)
 
-while idx<M and ioi in S:
-    left = S.index(ioi) # 글자가 나오는 시점
-    idx = left+len(ioi)-1
-    cnt+=1
-    
-    S = list(S)
-    S[left] = '#'    # 중복되어서 판별해야 하므로 확인한 지점만 체크
-    S = ''.join(S) # list를 공백없이 str로 만듦
-
-
-print(f"{cnt}\n")
-
-
+ans = 0
+for i in range(m-len(IOI)+1):
+    if s[i:i+len(IOI)] == IOI:
+        ans+=1
+print(ans)
