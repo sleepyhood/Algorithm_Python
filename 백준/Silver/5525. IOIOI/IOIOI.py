aa5685e2871fbacc5d8728b2a007a81242448c79
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2024-10-21 21:57:16 +0900

    [Silver I] Title: IOIOI, Time: 360 ms, Memory: 36640 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -13,11 +13,32 @@ from collections import deque
 #input = sys.stdin.readline
 #print = sys.stdout.write
 
-n = int(input())
-m = int(input())
+n = int(input())    # "IO" 패턴의 개수
+m = int(input())    # 문자열의 길이
 #li = list(map(int, input().split()))
-s = input()
+s = input()         # 주어진 문자열
 
+ans = 0
+cnt = 0
+i = 0
+
+# 슬라이딩 윈도우를 사용하여 찾기
+while i < m-1:
+    if s[i:i+3] == "IOI":
+        cnt += 1
+        if cnt == n:
+            ans+=1
+            cnt -= 1
+        i+=2
+
+    else:
+        cnt = 0
+        i+=1
+
+print(ans)
+
+"""기존 방식: 문자열 그대로 찾기"""
+"""
 IOI = ""
 
 for _ in range(n):
@@ -25,8 +46,10 @@ for _ in range(n):
 IOI+="I"
 #print(IOI)
 
-ans = 0
 for i in range(m-len(IOI)+1):
+    if s[i]==s[i+1]:
+        continue
     if s[i:i+len(IOI)] == IOI:
         ans+=1
 print(ans)
+"""
