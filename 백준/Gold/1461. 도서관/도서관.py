5272ca53bac6506193299c7191ef3b813173a96b
Seungwon Oh <69490791+sleepyhood@users.noreply.github.com>
2025-05-12 23:53:18 +0900

    [Gold IV] Title: 도서관, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -1,23 +1,6 @@
-"""
-[131]
-11*2	= 22
-6*2	    = 12
-29*2	= 58
-39
-
-
-[158]
-45*2    = 90
-9*2     = 18
-25*2    = 50
-"""
 n, m = map(int, input().split())
 li = list(map(int, input().split()))
 li.sort()
-
-if n == 1:
-    print(1)
-    exit()
 
 
 # 왕복 거리가 있으므로, 상대적으로 가까운곳부터 방문하는것이 최선
@@ -47,9 +30,3 @@ dist = [abs(i) for i in li]
 
 total-= max(dist)
 print(total)
-
-
-"""
-7 2
--37 2 -6 -39 -29 11 -28
-"""
